4c797aee4f4aa225a312284bd84f983cf11b7962
Willian Luis Zuqui <willianluiszuqui@gmail.com>
2023-01-15 22:34:54 +0000

    initial config with vite

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { ViteAngularPlugin } from '@nxext/angular-vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: './src',
+  plugins: [
+    ViteAngularPlugin({
+      target: 'es2020'
+    })
+  ],
+  resolve: {
+    preserveSymlinks: true
+  }
+})
